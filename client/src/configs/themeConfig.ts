9098f44fccce8ef8c4697ea59fbac13428abaaf2
Ngọc Hùng <huynhngochung12092001@gmail.com>
2024-11-13 22:25:15 +0700

    init source next

---
+++ client/src/configs/themeConfig.ts
@@ -0,0 +1,13 @@
+export type ThemeConfig = {
+	app_name: string;
+	home_page_url: string;
+	disableRipple: boolean;
+};
+
+const themeConfig: ThemeConfig = {
+	app_name: "wiHelper",
+	home_page_url: "/",
+	disableRipple: true,
+};
+
+export default themeConfig;
